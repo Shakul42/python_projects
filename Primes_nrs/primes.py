0a714ceadba6d48128bb5a7d5df387624104b2bc
Shakul42 <shakul.nicnicram@gmail.com>
2025-10-25 19:17:09 +0200

    Optimalization refactor in While loop

--- Primes_nrs/primes.py
+++ Primes_nrs/primes.py
@@ -30,7 +30,7 @@ def calcPrime(line):
     pocetCislic = len(line)
     overCislo = int(line)
     m = sqrt(overCislo) #stačí overiť deliteľov nižších ako druhá odmocnina
-    stN = int(line[len(line)-1])  # posledná číslica
+    stN = int(line[-1])  # posledná číslica
 
     # Overovanie prvočíselnosti
     if stN == 5 and overCislo != 5:
@@ -42,16 +42,17 @@ def calcPrime(line):
     elif sum_digits3(overCislo, pocetCislic) % 3 == 0:  # ciferný súčet je deliteľný 3
         print("Číslo nieje prvočíslo. Je deliteľné 3")
     else:
-        x, y, z, tri = 0, 0, 1, 0
-        while x < m:
-            y += 1
+        n, y, tri = 0, 0, 3
+        while n < m:
+            y += 1  # počet loopov (pre nepárne čísla)
             x = 1+2*y #nepárne čísla
-            tri = z*3   #násobky 3
+            n = x + 2   #next odd nr, pre porovnanie s odmocninou, lebo +2 je
+                        #rýchlejšie než zbytočné delenie delenie(last loop s x, bez n) 
             five = str(x)
             if x == tri: #SKIP delenia nepárnymi násobkami 3
-                z = z+2 #každé (1+2x)*3 je nepárne a deliteľné 3
+                tri = tri + 6 #každé (1+2x)*3 je nepárne a deliteľné 3
                 continue
-            elif int(five[len(five)-1]) == 5: #SKIP posledná číslica je 5
+            elif int(five[-1]) == 5: #SKIP posledná číslica je 5
                 continue
             elif overCislo % x == 0:  #deliteľnosť zvyšnými nepárnymi číslami
                 print("Číslo nieje prvočíslo. Je deliteľné " + str(x))         
